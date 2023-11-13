7e64f99b512db906d9bf7d376146d35287ee21fb
prince ajuzie <princeajuzie1@gmail.com>
2023-11-13 10:33:57 +0100

    unit test on the homepage

--- src/app/__tests__/Home.spec.js
+++ src/app/__tests__/Home.spec.js
@@ -9,5 +9,22 @@ describe('Home page - Rendering', ()=>{
     it('should have button with text clilck me', ()=>{
         render(<Home />);
         screen.getByRole('button', {name: "Click me"})
+    });
+    it('should have input field with label enter random text', ()=>{
+        render(<Home />);
+        screen.getByLabelText(/Enter Random text/)
+    })
+    it('should have input field with label enter specificTest', ()=>{
+        render(<Home />);
+        screen.getByLabelText(/Enter specific text/)
+    })
+    it('should find input field by pllaceholder text search', ()=>{
+      render(<Home />)
+      screen.getByPlaceholderText("search..")
+    }) 
+
+    it('should find input by display value',()=>{
+        render(<Home />)
+        screen.getByDisplayValue(/audi/)
     })
 })
--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,19 @@ export default function Home() {
   <label htmlFor="randomText">Enter Random text</label>
   <input type="text"  id="randomText"/>
 </div>
+<div>
+  <label htmlFor="specificTest">Enter specific text</label>
+  <input type="text"  id="specificTest"/>
+</div>
+<div>
+
+  <input  placeholder="search.."/>
+</div>
+<div>
+
+  <input value="audi"/>
+</div>
 </>
   )
 }
+ 
